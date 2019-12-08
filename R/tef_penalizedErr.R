216257ff70b6ab050a2de3aac0b923fb9567d0af
akcochrane <3661379@gmail.com>
2019-12-08 10:41:25 -0600

    update documentation

--- R/tef_penalizedErr.R
+++ R/tef_penalizedErr.R
@@ -1,12 +1,13 @@
 #' Penalize error for being close to a boundary
 #'
-#' Formally, `pErr=Err/dbeta(par,dbeta_penalty,dbeta_penalty)`
+#' Formally, `pErr=Err/dbeta(par,dbeta_penalty,dbeta_penalty)`, where
+#' par is the rate parameter normalized between 0 [lower bound] and 1 [upper bound]
 #'
 #' @param boundedPar Value of the parameter that has bounds.
 #' @param errPar Value of the error to be penalized.
 #' @param loBound Lower parameter boundary.
 #' @param upBound Upper parameter boundary.
-#' @param dbeta_penalty The multiplicative error penalty at approximately 5.3% away from the boundary. Error increases with increasing proximity to a bound.
+#' @param dbeta_penalty The multiplicative error penalty at approximately 5.3 percent away from the boundary. Error increases with increasing proximity to a bound.
 #'
 #' @export
 #'
