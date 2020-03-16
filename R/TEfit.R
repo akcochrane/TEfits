0b4c324bf0ca1478b33889970fbde7688279b26a
akcochrane <3661379@gmail.com>
2020-03-16 18:17:33 -0500

    Create tutorial vignette

--- R/TEfit.R
+++ R/TEfit.R
@@ -69,7 +69,7 @@
 #' @param errFun  A string defining an error function (e.g., 'ols', 'logcosh', 'bernoulli').
 #' @param changeFun A string defining the functional form of change (e.g., 'expo', 'power', 'weibull')
 #' @param bootPars A list defining the details for bootstrapped fits. Defaults to no bootstrapping. Necessary for estimates of uncertainty around fits and for covariance between parameters.
-#' @param blockTimeVar A vector defining the time points of sub-scales (e.g., "blocks" of times within the overall timescale of data collection)
+#' @param blockTimeVar A string identifying which covariate is the time points of sub-scales (e.g., "blocks" of times within the overall timescale of data collection)
 #' @param covarTerms An optional list of logical vectors indicating whether parameters should vary by covariates. See examples.
 #' @param control A list of model parameters. Use of tef_control() is highly recommended.
 
@@ -164,6 +164,9 @@ TEfit <- function(varIn,
                   covarTerms = list(),
                   control=tef_control()
 ){
+
+  # to do:
+  # # ensure, for expo_bl, that threshBlScale is bounded to positive reals (right?)
 
   modList <- list()
   modList$times <- c()
