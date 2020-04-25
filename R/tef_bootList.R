acab865fdecfc959dbb5612f78c216687797c6a3
akcochrane <3661379@gmail.com>
2020-04-25 13:19:39 -0500

    Add tef_lm for simple addition of nonlinear time-evolving predictors to OLS models

--- R/tef_bootList.R
+++ R/tef_bootList.R
@@ -2,7 +2,7 @@
 
 #' Make a list of parameters for resampling a TEfit model
 #'
-#' @param resamples   Number of resamples, in multiples of 10. More (e.g., >2000) is better, but requires more time and memory.
+#' @param resamples   Number of resamples. More (e.g., >2000) is better, but requires more time and memory.
 #' @param bootPercent Proportion of data to resample. Must be 0 < bootPercent <= 1. If 1, data is resampled repeatedly with replacement. If less than 1, that proportion of data is repeatedly randomly selected, the model is fit on the selected data, and fit indices (e.g., error) are calculated for the model predictions on the left-out data (i.e., cross-validation).
 #' @param bootTries   Number of optimization runs for each resample
 #'
